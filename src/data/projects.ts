4cff8de89c4a2010f6fd89fccbbf10569a42b161
bluesky2006 <simon.busby@gmail.com>
2025-07-25 22:27:04 +0100

    Update project description

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -20,7 +20,7 @@ export const projects = [
     },
     {
       title: "glitterball",
-      description: "glitterball is a resurrection of a club night I co-ran in London nearly a decade ago. As part of our revival, I gave the brand a fresh look inspired by 80s/90s rave flyers and vintage stock photography, drawing assets from archives like the Internet Archive.\n\n The site began life as a “vibe-coding” experiment using ChatGPT before I formally trained as a developer. Early tinkering with HTML/CSS and JS planted the seed that eventually led me to pursue software development full-time.",
+      description: "glitterball is the resurrection of a club night I co-ran in London that ended in 2015. As part of our revival, I gave the brand a fresh look inspired by 80s/90s rave flyers and vintage stock photography, drawing assets from archives like the Internet Archive.\n\n The site began life as a “vibe-coding” experiment using ChatGPT before I formally trained as a developer. Early tinkering with HTML/CSS and JS planted the seed that eventually led me to pursue software development full-time.",
       stack: ["HTML", "CSS", "JavaScript"],
       link: [
         { label: "Live site", url: "https://glitterball.info" },
