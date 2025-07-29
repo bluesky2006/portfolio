c880b57b0f54387307f7f69f0f7b9ad469c67159
bluesky2006 <simon.busby@gmail.com>
2025-07-29 13:25:53 +0100

    Add employee logo

--- src/components/ExperienceCards.tsx
+++ src/components/ExperienceCards.tsx
@@ -1,26 +1,34 @@
 import { experience } from "@/data/experience";
+import Image from "next/image";
 
 function ExperienceCards() {
   return (
-    <div className="flex flex-col gap-15">
-      {experience.map((exp, index) => {
-        const { title, company, description, date } = exp;
-        return (
-          <div
-            key={index}
-            className="border-brand-main border-l-4 px-5 py-1 text-white drop-shadow-sm transition delay-50 duration-300 hover:translate-x-2"
-          >
-            <div className="flex flew-row justify-between items-end mb-4">
-              <h2 className="text-brand-main">{title}</h2>
-              <p className="text-brand-main tracking-widest mb-0">{date}</p>
+    <div>
+      <div className="flex flex-col gap-10">
+        {experience.map((exp, index) => {
+          const { title, company, description, date, logo } = exp;
+          return (
+            <div
+              key={index}
+              className="border-brand-main border-l-4 px-5 py-1 text-white drop-shadow-sm transition delay-50 duration-300 hover:translate-x-2"
+            >
+              <Image
+                src={logo}
+                width={70}
+                height={70}
+                alt="logo"
+                className="absolute right-0"
+              />
+              <h2 className="text-brand-main mb-4">{title}</h2>
+              <h3 className="whitespace-pre-line mb-4 italic text-sm">
+                {company}
+              </h3>
+              <p className="text-brand-main">{date}</p>
+              <p className="whitespace-pre-line">{description}</p>
             </div>
-            <h3 className="whitespace-pre-line mb-4 italic text-sm">
-              {company}
-            </h3>
-            <p className="whitespace-pre-line">{description}</p>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
