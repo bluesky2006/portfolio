bc36e1603c8c9210515147f7372ba0418f8bf5e5
bluesky2006 <simon.busby@gmail.com>
2025-07-29 11:12:01 +0100

    Add experience data file

---
+++ src/data/experience.js
@@ -0,0 +1,16 @@
+export const experience = [
+  {
+    title: "Javascript Software Development Bootcamp",
+    company: "Northcoders",
+    date: "04–07.2025",
+    description:
+      "Intensive full-time bootcamp focused on full-stack JavaScript development. Built scalable web and mobile apps using Agile methodologies, pair programming, and TDD throughout. Emphasis was placed on collaboration, clean code practices, and practical debugging skills./n/nCore technologies used:/nJavaScript, HTML/CSS, React, React Native, Node.js, Express.js, PostgreSQL (PSQL), MongoDB (NoSQL), REST APIS, CRUD, MVC architecture, Jest, and Supertest./n/n Tooling and platforms included:/nGit, GitHub, Vite, Render, Supabase, Netlify, Expo, and the Gemini API.",
+  },
+  {
+    title: "Production Lead",
+    company: "Pip Decks",
+    date: "08.2023–07.2024",
+    description:
+      "Played a key role in bridging print and digital design. Developed a robust XML workflow to streamline the transfer of print content into the company’s app, improving efficiency and accuracy. Rebuilt the master InDesign template for their card products, ensuring consistency and ease of use across new releases./n/n Responsibilities also included applying artwork to packaging nets, helping to concept new print products like notebooks and workbooks, and creating brand-consistent designs for Miro board products. Also provided feedback that directly influenced updates to core content, proofread materials, updated the house style guide, and upheld best design practices to enhance the quality and consistency of all digital assets.",
+  },
+];
