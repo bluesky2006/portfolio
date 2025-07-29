e13d7312bc7dad29efb26b86650d4ba843ce2867
bluesky2006 <simon.busby@gmail.com>
2025-07-29 13:24:58 +0100

    Add discrete Skills component (currently unused)

---
+++ src/components/Skills.tsx
@@ -0,0 +1,28 @@
+import { skills } from "@/data/skills";
+import Pill from "./Pill";
+
+function Skills() {
+  const { programming, design } = skills;
+  return (
+    <section className="flex flex-col gap-10 my-5 mr-15">
+      <div>
+        <p>Programming</p>
+        <div className="flex flex-row gap-2 flex-wrap">
+          {programming.map((skill) => (
+            <Pill key={skill}>{skill}</Pill>
+          ))}
+        </div>
+      </div>
+      <div>
+        <p>Design</p>
+        <div className="flex flex-row gap-2 flex-wrap">
+          {design.map((skill) => (
+            <Pill key={skill}>{skill}</Pill>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
