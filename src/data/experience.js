5abb90bf6355820ba45babe932930b539b8ef48d
bluesky2006 <simon.busby@gmail.com>
2025-07-29 13:25:29 +0100

    Update experience data

--- src/data/experience.js
+++ src/data/experience.js
@@ -2,15 +2,17 @@ export const experience = [
   {
     title: "Javascript Software Development Bootcamp",
     company: "Northcoders",
-    date: "04–07.2025",
+    date: "April–July 2025",
     description:
-      "Intensive full-time bootcamp focused on full-stack JavaScript development. Built scalable web and mobile apps using Agile methodologies, pair programming, and TDD throughout. Emphasis was placed on collaboration, clean code practices, and practical debugging skills./n/nCore technologies used:/nJavaScript, HTML/CSS, React, React Native, Node.js, Express.js, PostgreSQL (PSQL), MongoDB (NoSQL), REST APIS, CRUD, MVC architecture, Jest, and Supertest./n/n Tooling and platforms included:/nGit, GitHub, Vite, Render, Supabase, Netlify, Expo, and the Gemini API.",
+      "I completed an intensive full-time bootcamp with Northcoders, focused on full-stack JavaScript development. The course emphasized Agile methodologies, pair programming, and test-driven development, with a strong focus on collaboration, clean code, and practical debugging skills. I built scalable web and mobile applications using core technologies such as JavaScript, HTML/CSS, React, React Native, Node.js, Express.js, PostgreSQL, MongoDB, and REST APIs, following the MVC architecture. Testing was carried out using Jest and Supertest. I also worked with a range of tools and platforms including Git, GitHub, Vite, Render, Supabase, Netlify, Expo, and the Gemini API, gaining experience in both frontend and backend development workflows.",
+    logo: "/nc_logo.png",
   },
   {
     title: "Production Lead",
     company: "Pip Decks",
-    date: "08.2023–07.2024",
+    date: "August 2023–July 2024",
     description:
-      "Played a key role in bridging print and digital design. Developed a robust XML workflow to streamline the transfer of print content into the company’s app, improving efficiency and accuracy. Rebuilt the master InDesign template for their card products, ensuring consistency and ease of use across new releases./n/n Responsibilities also included applying artwork to packaging nets, helping to concept new print products like notebooks and workbooks, and creating brand-consistent designs for Miro board products. Also provided feedback that directly influenced updates to core content, proofread materials, updated the house style guide, and upheld best design practices to enhance the quality and consistency of all digital assets.",
+      "As Production Lead at Pip Decks, I played a key role in bridging print and digital design. I developed a robust XML workflow to streamline the transfer of print content into the company’s app, improving efficiency and accuracy. I rebuilt the master InDesign template for their card products, ensuring consistency and ease of use across new releases. My responsibilities also included applying artwork to packaging nets, helping to concept new print products like notebooks and workbooks, and creating brand-consistent designs for Miro board products. I provided feedback that directly influenced updates to core content, proofread materials, updated the house style guide, and upheld best design practices to enhance the quality and consistency of all digital assets.",
+    logo: "/pd.png",
   },
 ];
