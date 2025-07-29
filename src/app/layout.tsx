90251ebdedce2eb7a46a766e6275fbc0e4c70aba
bluesky2006 <simon.busby@gmail.com>
2025-07-29 13:50:26 +0100

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "../styles/globals.css";
 export const metadata: Metadata = {
   title: "Simon Busby | Portfolio",
   description: "Built with Next.js",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
