5aaaa9fe5830c146bf4b65f781d7516b2318807b
bluesky2006 <simon.busby@gmail.com>
2025-07-29 13:25:36 +0100

    Update projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,8 @@
 export const projects = [
   {
     title: "portfolio",
-    date: "07.2025",
-    summary: "This very website.",
+    date: "July 2025",
+    summary: "This very website",
     description:
       "A simple, styled website I built to show-off my portfolio work and experience, and learn a few new skills (in this case, Tailwind and Next.js).",
     stack: ["Next.js", "Tailwind"],
@@ -10,8 +10,8 @@ export const projects = [
   },
   {
     title: "reci-p.ai",
-    date: "07.2025",
-    summary: "AI-powered app that reverse engineers recipes from food labels.",
+    date: "July 2025",
+    summary: "AI-powered app that reverse engineers recipes from food labels",
     description:
       "reci-p.ai is an app for Android and iOS that provides users with the ability to quickly and easily recreate recipes of their favourite shop-bought food items at the tap of a button. Users can take or upload photos of their food and receive great tasting recipes that can be made at home from fresh. Recipes can be saved and favourited, and viewed across devices. We wanted users to get their recipes in as few steps as possible, with a clean and easy to understand UI. As such, the app also integrates Google Sign-in so users can get started as quickly as possible, and leverages Google Gemini to provide an accurate and quick response.",
     stack: ["React Native", "Expo", "Gemini API", "MongoDB"],
@@ -25,9 +25,9 @@ export const projects = [
   },
   {
     title: "northcoders news",
-    date: "06.2025",
+    date: "June 2025",
     summary:
-      "A full-stack news aggregator site mimicking Hacker News with voting, commenting and sorting.",
+      "A full-stack news aggregator site mimicking Hacker News with voting, commenting and sorting",
     description:
       "The Northcoder News app offers users a streamlined experience of interacting with a simple, Reddit-style news website. Built as part of the Northcoders bootcamp, it allows users to browse a list of articles, filter them by topic, and sort them by values such as date, title, user, or votes. Users can view individual articles, post comments, and delete their own.\n\n The app is powered by the NC News API, a RESTful backend developed using Node.js, Express, and PostgreSQL. It showcases key backend skills including database schema design, SQL querying, Express routing, error handling and data validation. The project also includes integration testing with Jest and Supertest, and is deployed to a live environment via Render.",
     stack: [
@@ -54,9 +54,9 @@ export const projects = [
   },
   {
     title: "glitterball",
-    date: "05.2025",
+    date: "May 2025",
     summary:
-      "A small, stylised website promoting glitterball – a club night I co-run in Somerset.",
+      "A small, stylised website promoting glitterball – a club night I co-run in Somerset",
     description:
       "glitterball is the resurrection of a club night I co-ran in London that ended in 2015. As part of our revival, I gave the brand a fresh look inspired by 80s/90s rave flyers and vintage stock photography, drawing assets from archives like the Internet Archive.\n\n The site began life as a “vibe-coding” experiment using ChatGPT before I formally trained as a developer. Early tinkering with HTML/CSS and JS planted the seed that eventually led me to pursue software development full-time.",
     stack: ["HTML", "CSS", "JavaScript"],
