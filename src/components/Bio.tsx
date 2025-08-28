0e2238bd0e5cdadfcc6daa7c03309d718fc5188b
bluesky2006 <simon.busby@gmail.com>
2025-08-28 08:17:37 +0100

    update bio

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -10,9 +10,9 @@ function Bio() {
       <div>
         <p>
           For over a decade, I worked in publishing, crafting layouts, wrangling type and obsessing
-          over margins and grids. I loved the tactility of design, the problem-solving in structure,
-          the satisfaction of clarity. But as the industry shifted and tools evolved, I found myself
-          drawn more and more to the how behind the screen – the logic, the interactivity and the
+          over margins and grids. I loved the tactility of prinnt design, the problem-solving in
+          structure, the satisfaction of clarity. But as the industry shifted and tools evolved, I
+          found myself drawn more and more to the screen – the logic, the interactivity and the
           code.
         </p>
         <p>
@@ -39,15 +39,14 @@ function Bio() {
           <strong className="hover:text-brand-main h-6 w-6 text-white transition-colors duration-300">
             MongoDB
           </strong>{" "}
-          – with a dash of AI and mobile thrown in for good measure. Along the way, I discovered
+          – with a dash of AI and mobile thrown in for good measure. And along the way, I discovered
           that many of the instincts I’d honed in design – clarity, usability, structure and
           communication – translate beautifully to code.
         </p>
         <p>
-          Now I’m looking to combine the best of both worlds: visual thinking and logical flow,
-          aesthetic polish and functional design. Whether I’m building an interface, debugging a
-          server or just trying to get that last media query to behave, I bring curiosity, craft and
-          a drive to make things that feel good to use.
+          Now I’m looking to combine the best of both worlds. Whether I’m building an interface,
+          debugging a server or just trying to get that last media query to behave, I bring
+          curiosity, craft and a drive to make things that feel good to use.
         </p>
         <p>
           Outside of work, you’ll find me digging for music, hosting the occasional club night, or
