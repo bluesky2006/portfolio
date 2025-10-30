ede5d143cf621acf4613dc3e7372182d47fddf3c
bluesky2006 <simon.busby@gmail.com>
2025-10-30 16:25:29 +0000

    give pills solid background

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,6 +1,6 @@
 function Pill({ children }: { children: React.ReactNode }) {
   return (
-    <span className="border-brand-main border-1 inline-block rounded-full px-2 py-1 text-xs font-normal  hover:bg-brand-main hover:text-white cursor-default">
+    <span className="border-brand-main border-1 inline-block rounded-full px-2 py-1 text-xs font-normal bg-[#fefdfc] hover:bg-brand-main hover:text-white cursor-default">
       {children}
     </span>
   );
