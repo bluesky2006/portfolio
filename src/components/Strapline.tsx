7eb936a4bb35fbc173cb3d28aac76e23b2662f5f
bluesky2006 <simon.busby@gmail.com>
2025-10-30 21:43:42 +0000

    add more colour

--- src/components/Strapline.tsx
+++ src/components/Strapline.tsx
@@ -4,12 +4,15 @@ function Strapline() {
       <h1 className="flex flex-col justify-start">
         <span className="text-brand-main text-3xl absolute -left-8 pt-2">&gt; </span>{" "}
         <div>
-          <span>simon.&#x200b;busby()</span>
-          <span className="animate-blink-text text-brand-main">_</span>
+          <span>
+            simon<span className="text-brand-main">.</span>&#x200b;busby
+            <span className="text-brand-main">()</span>
+          </span>
+          <span className="animate-blink-text">_</span>
         </div>
       </h1>
       <div className="flex flex-col gap-3">
-        <h2>Junior software developer</h2>
+        <h2 className="text-brand-main">Junior software developer</h2>
         <h3>Building engaging, beautiful digital products</h3>
         <p className="mt-12 mb-16 mr-15">
           I&apos;m a{" "}
