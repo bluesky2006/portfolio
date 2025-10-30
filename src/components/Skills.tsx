ef72bb9e5087c2f630b7d32456a2fd89d1ebe979
bluesky2006 <simon.busby@gmail.com>
2025-10-30 15:36:04 +0000

    comment out design section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,21 +6,21 @@ function Skills() {
   return (
     <section className="flex flex-col gap-10 my-5 mr-15">
       <div>
-        <p>Programming</p>
+        {/* <p>Programming</p> */}
         <div className="flex flex-row gap-2 flex-wrap">
           {programming.map((skill) => (
             <Pill key={skill}>{skill}</Pill>
           ))}
         </div>
       </div>
-      <div>
+      {/* <div>
         <p>Design</p>
         <div className="flex flex-row gap-2 flex-wrap">
           {design.map((skill) => (
             <Pill key={skill}>{skill}</Pill>
           ))}
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
