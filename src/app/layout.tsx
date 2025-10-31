b59577cb22f3e271f146645b1eaac649783767f8
bluesky2006 <simon.busby@gmail.com>
2025-10-31 14:33:54 +0000

    tweak gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className="relative mx-auto min-h-screen max-w-screen-xl bg-transparent">
         {/* overlay gradient */}
-        <div className="fixed inset-0 -z-5 pointer-events-none bg-gradient-to-r from-brand-white/10 via-brand-white/80 to-brand-white dark:from-brand-black/10 dark:via-brand-black/80 dark:to-brand-black" />
+        <div className="fixed inset-0 -z-5 pointer-events-none bg-gradient-to-r from-brand-white/0 via-brand-white/80 to-brand-white dark:from-brand-black/0 dark:via-brand-black/80 dark:to-brand-black" />
         {children}
       </body>
     </html>
