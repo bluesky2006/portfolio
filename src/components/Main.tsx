187fa7174c85e1252dfe4c797aca9745377b80c0
bluesky2006 <simon.busby@gmail.com>
2025-10-31 14:22:09 +0000

    adjust padding and margins

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,13 +6,13 @@ import Nav from "./Nav";
 
 function Main() {
   return (
-    <main className="flex w-[100%] md:w-[70%] lg:w-[50%] flex-col gap-20 pt-17 lg:pb-12">
+    <main className="flex w-[100%] md:w-[70%] lg:w-[50%] flex-col gap-26 lg:pb-12">
       <aside className="hidden md:block lg:hidden fixed right-20 top-12">
         <Nav />
       </aside>
 
       <section id="bio">
-        <h2 className="py-10 text-brand-main font-bold tracking-wide">bio</h2>
+        <h2 className="pb-10 pt-26 text-brand-main font-bold tracking-wide">bio</h2>
         <Bio />
       </section>
 
