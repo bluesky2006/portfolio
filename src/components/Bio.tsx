3d9a76e8d56ae1e0116658003a2390bccabc0a60
bluesky2006 <simon.busby@gmail.com>
2025-10-31 14:22:21 +0000

    make bio snappier

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -5,27 +5,33 @@ function Bio() {
     <div>
       <p>
         For over a decade, I worked in publishing, crafting layouts, wrangling type and obsessing
-        over margins and grids. I loved the tactility of print design, the problem-solving in
-        structure, the satisfaction of clarity. But as the industry shifted and tools evolved, I
-        found myself drawn more and more to the screen – the logic, the interactivity and the code.
+        over margins and grids. I loved the tactility of print design and solving problems with
+        structure – but as the industry shifted, I found myself drawn to code.
       </p>
       <p>
-        What began as a few tentative tweaks to a website soon snowballed into a full immersion in
-        full-stack JavaScript development. I completed an intensive bootcamp with{" "}
-        <a href="https://www.northcoders.com/" target="_blank">
-          <span className="hover:text-brand-main h-6 w-6 transition-colors duration-300">
+        What began as a few tentative tweaks to{" "}
+        <a href="https://glitterball.info/" target="_blank">
+          <span className="hover:text-brand-main h-6 w-6 transition-colors duration-300 font-semibold">
+            a website
+          </span>
+        </a>{" "}
+        soon snowballed into full immersion in full-stack JavaScript development. I completed an
+        intensive bootcamp with{" "}
+        <a
+          href="https://www.northcoders.com/our-courses/software-development-bootcamp-in-javascript/"
+          target="_blank"
+        >
+          <span className="hover:text-brand-main h-6 w-6 transition-colors duration-300 font-semibold">
             Northcoders
           </span>
-        </a>
-        , where I built projects using React, Node.js, PostgreSQL and MongoDB – with a dash of AI
-        and mobile thrown in for good measure. And along the way, I discovered that many of the
-        instincts I’d honed in design – clarity, usability, structure and communication – translate
-        beautifully to code.
+        </a>{" "}
+        and discovered along the way that many of the instincts I’d honed in design – clarity,
+        usability, structure and communication – translate beautifully to&nbsp;code.
       </p>
       <p>
-        Now I’m looking to combine the best of both worlds. Whether I’m building an interface,
-        debugging a server or just trying to get that last media query to behave, I bring curiosity,
-        craft and a drive to make things that feel good to use.
+        Now I’m looking to combine the best of both worlds. Whether I’m finessing the spacing in a
+        new interface, debugging a server or just trying to get that last media query to behave, I
+        bring curiosity, craft and a drive to make things that feel good to&nbsp;use.
       </p>
       <p className="mb-12">
         Outside of work, you’ll find me digging for music, hosting the occasional club night, or
