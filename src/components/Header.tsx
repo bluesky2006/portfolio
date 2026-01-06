8054e233fc856d65d1e24e83c4438057105628be
bluesky2006 <simon.busby@gmail.com>
2026-01-06 14:54:55 +0000

    move skills to header and hide nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,17 @@
 import Socials from "./Socials";
 import Footer from "./Footer";
-import Nav from "./Nav";
 import Strapline from "./Strapline";
+import Skills from "./Skills";
 
 function Header() {
   return (
     <header className="lg:sticky lg:top-24 lg:pt-0 pt-12 flex lg:h-[80vh] w-[100%] md:w-[70%] lg:w-[50%] flex-col lg:justify-between">
       <div>
         <Strapline />
-        <aside className="hidden lg:block">
+        <Skills />
+        {/* <aside className="hidden lg:block">
           <Nav />
-        </aside>
+        </aside> */}
       </div>
       <div>
         <Footer />
